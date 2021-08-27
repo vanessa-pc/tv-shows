10b3cfbe896275980ea03065fa0da226cc1415da
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-27 14:11:06 +0300

    Added components folder, episodelist component, imported episodes.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { greet } from "./utils/greet";
+import episodes from './episodes.json'
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
   return <h1>{greet("World")}</h1>;
---
+++ src/components/Episodes.tsx
@@ -0,0 +1,5 @@
+export function EpisodeList() :JSX.Element {
+    return (
+        <h1>hello</h1>
+    )
+}
